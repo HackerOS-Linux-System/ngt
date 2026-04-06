d234d2851130f1672c5a40de83f32186364c96db
michal92299 <chudybyczek2137@gmail.com>
2026-04-06 20:40:27 +0200

    Update model.go

--- source-code/src/model.go
+++ source-code/src/model.go
@@ -16,7 +16,7 @@ import (
 
 type item struct {
 	title, desc string
-	status      string // For Git status
+	status      string
 	selected    bool
 	isDir       bool
 	size        int64
@@ -25,27 +25,51 @@ type item struct {
 
 func (i item) Title() string {
 	title := i.title
+	prefix := "  "
 	if i.selected {
-		title = "* " + title
+		prefix = selectedMarker + " "
+	}
+	if i.isDir {
+		title = dirStyle.Render(title + "/")
+	} else {
+		title = fileStyle.Render(title)
 	}
 	if i.status != "" {
 		switch i.status[0] {
 			case 'M':
-				title = gitModifiedStyle.Render(title)
+				title = gitModifiedStyle.Render(i.title)
 			case 'A':
-				title = gitAddedStyle.Render(title)
+				title = gitAddedStyle.Render(i.title)
 			case 'D':
-				title = gitDeletedStyle.Render(title)
+				title = gitDeletedStyle.Render(i.title)
 		}
 	}
-	return title
+	return prefix + title
 }
 
 func (i item) Description() string {
-	return fmt.Sprintf("%s | Size: %d | Mod: %s", i.desc, i.size, i.modTime.Format("2006-01-02 15:04"))
+	kind := "file"
+	if i.isDir {
+		kind = "dir "
+	}
+	sizeStr := humanSize(i.size)
+	return fmt.Sprintf("%s │ %s │ %s", kind, sizeStr, i.modTime.Format("2006-01-02 15:04"))
 }
 
 func (i item) FilterValue() string { return i.title }
+
+func humanSize(s int64) string {
+	const unit = 1024
+	if s < unit {
+		return fmt.Sprintf("%d B", s)
+	}
+	div, exp := int64(unit), 0
+	for n := s / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(s)/float64(div), "KMGTPE"[exp])
+}
 
 type CommandResult struct {
 	Output string
@@ -58,6 +82,20 @@ type ProgressMsg struct {
 
 type tickMsg time.Time
 
+// SortMode defines how file list is sorted
+type SortMode int
+
+const (
+	SortByName SortMode = iota
+	SortBySize
+	SortByDate
+	SortByExt
+)
+
+func (s SortMode) String() string {
+	return [...]string{"Name", "Size", "Date", "Ext"}[s]
+}
+
 type panel struct {
 	currentDir    string
 	gitBranch     string
@@ -65,55 +103,99 @@ type panel struct {
 	selectedFiles map[string]bool
 	preview       viewport.Model
 	vfs           vfsHandler
+	sortMode      SortMode
 }
 
 type Model struct {
-	panels         [2]panel
-	activePanel    int
-	commandInput   textinput.Model
-	statusMsg      string
-	keys           keyMap
-	mode           mode
-	editor         textarea.Model
-	editorFile     string
-	progress       progress.Model
-	quitting       bool
-	ProgressChan   chan ProgressMsg
-	ResultChan     chan CommandResult
-	fuzzyInput     textinput.Model
-	fuzzyResults   []string
+	panels       [2]panel
+	activePanel  int
+	commandInput textinput.Model
+	statusMsg    string
+	keys         keyMap
+	mode         mode
+
+	editor     textarea.Model
+	editorFile string
+
+	progress progress.Model
+	quitting bool
+
+	ProgressChan chan ProgressMsg
+	ResultChan   chan CommandResult
+
+	fuzzyInput   textinput.Model
+	fuzzyResults []string
+
 	bulkRenameFrom string
 	bulkRenameTo   string
-	subShell       bool
+
+	// confirmation dialog
+	confirmMsg    string
+	confirmAction func()
+
+	// podman browser
+	podmanContainers []string
+
+	subShell bool
+
+	termW int
+	termH int
 }
 
 func InitialModel() Model {
 	wd, _ := os.Getwd()
+
 	ti := textinput.New()
-	ti.Placeholder = "Enter command (e.g., cd .., mv file1 file2, sftp://user:pass@host)"
+	ti.Placeholder = "cmd: cd, cp, mv, rm, mkdir, touch, sftp, podman, hedit…"
 	ti.Focus()
 	ti.Width = 80
+
 	del := list.NewDefaultDelegate()
-	del.Styles.SelectedTitle = del.Styles.SelectedTitle.Foreground(lipgloss.Color("#FFFFFF")).Bold(true)
-	del.Styles.NormalTitle = del.Styles.NormalTitle.Foreground(lipgloss.Color("#CCCCCC"))
+	del.Styles.SelectedTitle = del.Styles.SelectedTitle.
+	Foreground(lipgloss.Color(colorAccent)).
+	Background(lipgloss.Color(colorSurface2)).
+	Bold(true).
+	BorderLeft(true).
+	BorderStyle(lipgloss.ThickBorder()).
+	BorderForeground(lipgloss.Color(colorAccent))
+	del.Styles.NormalTitle = del.Styles.NormalTitle.
+	Foreground(lipgloss.Color(colorText))
+	del.Styles.SelectedDesc = del.Styles.SelectedDesc.
+	Foreground(lipgloss.Color(colorMuted)).
+	Background(lipgloss.Color(colorSurface2))
+	del.Styles.NormalDesc = del.Styles.NormalDesc.
+	Foreground(lipgloss.Color(colorMuted))
+	del.ShowDescription = true
+
 	l1 := list.New([]list.Item{}, del, 0, 0)
-	l1.Title = "Left Panel"
-	l1.Styles.Title = subtitleStyle
+	l1.Title = "● Left"
+	l1.Styles.Title = panelTitleStyle
 	l1.SetShowFilter(true)
+	l1.SetFilteringEnabled(true)
+
 	l2 := list.New([]list.Item{}, del, 0, 0)
-	l2.Title = "Right Panel"
-	l2.Styles.Title = subtitleStyle
+	l2.Title = "○ Right"
+	l2.Styles.Title = panelTitleStyle
 	l2.SetShowFilter(true)
+	l2.SetFilteringEnabled(true)
+
 	ta := textarea.New()
-	ta.Placeholder = "Edit your file here..."
+	ta.Placeholder = "Edit your file here…"
 	ta.Focus()
+
 	pv1 := viewport.New(0, 0)
-	pv1.SetContent("Preview")
+	pv1.SetContent("Select a file to preview")
 	pv2 := viewport.New(0, 0)
-	pv2.SetContent("Preview")
-	prog := progress.New(progress.WithDefaultGradient())
+	pv2.SetContent("Select a file to preview")
+
+	prog := progress.New(
+		progress.WithGradient(colorAccent, colorSuccess),
+			     progress.WithoutPercentage(),
+	)
+
 	fi := textinput.New()
-	fi.Placeholder = "Fuzzy search..."
+	fi.Placeholder = "Fuzzy search…"
+
 	m := Model{
 		panels: [2]panel{
 			{currentDir: wd, fileList: l1, selectedFiles: make(map[string]bool), preview: pv1, vfs: localVFS{}},
@@ -125,8 +207,8 @@ func InitialModel() Model {
 		mode:         explorerMode,
 		editor:       ta,
 		progress:     prog,
-		ProgressChan: make(chan ProgressMsg),
-		ResultChan:   make(chan CommandResult),
+		ProgressChan: make(chan ProgressMsg, 10),
+		ResultChan:   make(chan CommandResult, 10),
 		fuzzyInput:   fi,
 	}
 	for i := range m.panels {
